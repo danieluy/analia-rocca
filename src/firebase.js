31eb1016c05f087fae53d5668868f77f08caedc9
Daniel Sosa <danielsosa.dev@gmail.com>
2018-04-08 18:58:35 -0300

    Adds firebase unavailable checks

--- src/firebase.js
+++ src/firebase.js
@@ -1,30 +1,46 @@
 /* global firebase */
+import { handleBackendError } from './utils';
+
+const firebaseUnavailable = {
+  status: 400,
+  response: {
+    body: {
+      message: 'The database is unavailabe.\nPlease try again later.'
+    }
+  }
+};
 
 const getCollections = () =>
   new Promise((resolve, reject) => {
-    firebase.database().ref('collections').once(
-      'value',
-      snapshot => resolve(snapshot.val()),
-      err => reject(err)
-    );
+    if (typeof firebase !== 'undefined')
+      firebase.database().ref('collections').once(
+        'value',
+        snapshot => resolve(snapshot.val()),
+        err => reject(err)
+      );
+    else
+      handleBackendError(firebaseUnavailable);
   });
 
 const populateCollections = collections =>
   Promise.all(collections
     .map((collection, i) => new Promise((resolve, reject) => {
-      firebase.database().ref('documents')
-        .orderByChild('collection')
-        .equalTo(i)
-        .once(
-          'value',
-          (snapshot) => {
-            let photos = snapshot.val();
-            if (!Array.prototype.isPrototypeOf(photos))
-              photos = Object.entries(photos).map(photo => photo[1]); // when firebase collection has only one item
-            resolve(Object.assign({}, collection, { photos }));
-          },
-          err => reject(err)
-        );
+      if (typeof firebase !== 'undefined')
+        firebase.database().ref('documents')
+          .orderByChild('collection')
+          .equalTo(i)
+          .once(
+            'value',
+            (snapshot) => {
+              let photos = snapshot.val();
+              if (!Array.prototype.isPrototypeOf(photos))
+                photos = Object.entries(photos).map(photo => photo[1]); // when firebase collection has only one item
+              resolve(Object.assign({}, collection, { photos }));
+            },
+            err => reject(err)
+          );
+      else
+        handleBackendError(firebaseUnavailable);
     })));
 
 export {
